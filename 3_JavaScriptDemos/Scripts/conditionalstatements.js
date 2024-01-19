077225d0ce4a2d027bcf3dab72e4887a4d834f22
vikul rathod <vikul.rathod.net@gmail.com>
2024-01-19 12:42:21 +0530

    javascript session 3

---
+++ 3_JavaScriptDemos/Scripts/conditionalstatements.js
@@ -0,0 +1,87 @@
+﻿var num = prompt('Please enter a number')
+
+document.write(`Input : ${num} <br/>`);
+
+if (num == 10) {
+    document.write(`TEN <br/>`);
+} else {
+    document.write(`NOT TEN <br/>`);
+}
+
+var r1 = (num == 10) ? 'TEN' : 'NOT TEN';
+document.write(`${r1} <br/>`);
+
+if (num == 10) {
+    document.write(`TEN <br/>`);
+} else if (num == 20) {
+    document.write(`TWENTY <br/>`);
+} else if (num == 30) {
+    document.write(`THIRTY <br/>`);
+} else {
+    document.write(`NOT 10 20 or 30 <br/>`);
+}
+
+document.write('------------- switch output -------------- <br/>');
+
+
+switch (parseInt(num)) {
+    case 10:
+        document.write(`TEN <br/>`);
+        break;
+    case 20:
+        document.write(`TWENTY <br/>`);
+        break;
+    case 30:
+        document.write(`THIRTY <br/>`);
+        break;
+    default:
+        document.write(`NOT 10 20 or 30 <br/>`);
+        break;
+}
+
+
+function funHighest() {
+
+    //var var1;
+    //alert(`var1 : ${var1}`); // undefined
+
+    var a = document.getElementById('first').value;
+    var b = document.getElementById('second').value;
+    var c = document.getElementById('third').value;
+
+    if (a == '' ||
+        b == '' ||
+        c == '') {
+        alert('You missed to specify input for one ot two');
+        return;
+    }
+
+    if (isNaN(a) || isNaN(b) || isNaN(c)) {
+        alert('Please enter valid number');
+        return;
+    }
+
+    a = parseInt(a);
+    b = parseInt(b);
+    c = parseInt(c);
+
+    if (a > b) {
+        if (a > c) {
+            document.getElementById('highest').value = a;
+        } else {
+            document.getElementById('highest').value = c;
+        }
+    } else if (b > a) {
+        if (b > c) {
+            document.getElementById('highest').value = b;
+        } else {
+            document.getElementById('highest').value = c;
+        }
+    } else {
+        if (c > a) {
+            document.getElementById('highest').value = c;
+        } else {
+            document.getElementById('highest').value = a;
+        }
+    }
+}
---
+++ 3_JavaScriptDemos/Scripts/operators.js
@@ -0,0 +1,66 @@
+﻿// alert('Operators JS Loaded');
+
+var a = 10, b = 10;
+
+var r1 = a == b;
+document.write(`a == b : ${r1} <br/>`);
+
+r1 = a != b;
+document.write(`a != b : ${r1} <br/>`);
+
+r1 = a <= b;
+document.write(`a <= b : ${r1} <br/>`);
+
+r1 = a === b;
+document.write(`a === b : ${r1} <br/>`);
+
+b = '10';
+r1 = a == b;
+document.write(`a == b : ${r1} <br/>`);
+
+r1 = a === b;
+document.write(`a === b : ${r1} <br/>`); // false
+
+b = 10; // Number type
+r1 = a === b;
+document.write(`a === b : ${r1} <br/>`); // true
+
+var c = 20;
+
+r1 = a == b && a < c;
+document.write(`a == b && a < c : ${r1} <br/>`);
+
+r1 = a == b && a > c;
+document.write(`a == b && a > c : ${r1} <br/>`);
+
+r1 = a == b || a > c;
+document.write(`a == b || a > c : ${r1} <br/>`);
+
+a++;
+document.write(`a++ : ${a} <br/>`); // 11
+
+// a = a + 10;
+a += 9;
+document.write(`a = a + 10 : ${a} <br/>`); // 21
+
+a = 10;
+b = 'vishal';
+document.write(`${a + b}  <br/>`);
+
+a = 15;
+b = 10;
+document.write(`${a / b} <br/>`);
+document.write(`${a % b} <br/>`);
+
+// type conversion
+var x = '10.5', y = '10';
+document.write(`${x + y} <br/>`);
+
+document.write(`${parseInt(x) + parseInt(y)} <br/>`);
+
+document.write(`${parseFloat(x) + parseFloat(y)} <br/>`);
+
+var s1 = 'false';
+
+r1 = Boolean(s1);
+document.write(`${r1} <br/>`);
