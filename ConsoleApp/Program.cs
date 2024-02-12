7a9359cdd1de2c573fbe993f76b70f441e734872
vikul rathod <vikul.rathod.net@gmail.com>
2024-02-12 12:52:10 +0530

    jquery sesion 6

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Sample s = new Sample();
+s.PrintA();
+s.PrintB(); // waiting to complete PrintA()
+
+Console.ReadLine();
+
+
+
+public class Sample
+{
+    public async Task PrintA()
+    {
+        await Task.Run(() =>
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"PrintA() : {i}");
+            }
+        });
+    }
+    public async void PrintB()
+    {
+        await Task.Run(() =>
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"PrintB() : {i}");
+            }
+        });
+    }
+}
